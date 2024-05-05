7e623a9d46d1e8c6489b15a897ccea319eca799b
“Saventiiy” <breadduckwithpigpeppa@gmail.com>
2024-05-06 00:27:05 +0200

    Added input validation

--- app/src/main/java/com/saventiy/windy_task/screens/main/MainScreen.kt
+++ app/src/main/java/com/saventiy/windy_task/screens/main/MainScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -16,13 +17,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 fun MainScreen(
-    modifier: Modifier = Modifier,
-    viewModel: MainViewModel = hiltViewModel()
+    modifier: Modifier = Modifier, viewModel: MainViewModel = hiltViewModel()
 ) {
     val uiState = viewModel.uiState.collectAsState()
     when (val result = uiState.value) {
@@ -42,13 +43,20 @@ fun MainView(modifier: Modifier = Modifier, viewModel: MainViewModel, sumStr: St
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
-                OutlinedTextField(
-                    modifier = modifier.fillMaxWidth(),
+                OutlinedTextField(modifier = modifier.fillMaxWidth(),
                     value = flowNumbers.value,
-                    onValueChange = {
-                        flowNumbers.value = it
+                    onValueChange = { value ->
+                        val number = value.toIntOrNull()
+                        if (number == null || (number in 0 until 10_000)) {
+                            flowNumbers.value = value
+                        }
                     },
-                )
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Number
+                    ),
+                    label = { Text(text = "Enter a value between 0 and 10.000") })
+
+                //pu-pu-pu i see a problem without blocking a button while flows are running
                 Button(onClick = { viewModel.startSummationFlow(flowNumbers.value.toInt()) }) {
                     Text(text = "Start")
                 }
